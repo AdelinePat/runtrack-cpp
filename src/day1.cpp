089ce57d341c63734ef27f9334a559f044788ba2
Adeline <adeline.patenne@hotmail.fr>
2025-11-03 15:59:35 +0100

    feat: job 8 done

--- src/day1.cpp
+++ src/day1.cpp
@@ -56,3 +56,23 @@ int revertNumber(int number) {
     }
     return reversed;
 }
+
+bool isLeapYear(int year) {
+    // 1. If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
+    // 2. If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
+    // 3. If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
+    // 4. The year is a leap year (it has 366 days).
+    // 5. The year is not a leap year (it has 365 days).
+    bool isLeap{false};
+    // if (year % 4 == 0 && year % 100 != 0) {
+    //     isLeap = true;
+    // } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+    //     isLeap = true;
+    // }
+    if (year % 4 == 0) {
+        if (year % 100 != 0 || year % 400 == 0) {
+            isLeap = true;
+        }
+    }
+    return isLeap;
+}
